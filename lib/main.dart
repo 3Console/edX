dbe37aa1334b999d3c8f21539a1d31a92e47dc75
3Console <quanghuy101202@gmail.com>
2020-06-14 13:47:29 +0700

    create auth service and setup wrapper

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp/pages/wrapper.dart';
+import 'package:flutterapp/models/user.dart';
+import 'package:provider/provider.dart';
+import 'package:flutterapp/services/auth.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Wrapper(),
-  ));
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
+    );
+  }
 }
