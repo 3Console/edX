412b8b204f91fccade7f4e96f345f1fac735d152
trungdinh98 <batrungdinh33@gmail.com>
2020-06-16 23:08:10 +0700

    Update Welcome screen

--- lib/pages/home/welcome.dart
+++ lib/pages/home/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutterapp/pages/authenticate/authenticate.dart';
 import 'package:flutterapp/pages/shared/constants.dart';
 
@@ -11,11 +12,21 @@ class WelcomePage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            SizedBox(height: 20),
-            Text('Welcome to edX'),
+            SizedBox(height: 50),
+            Text("Courses from the world's best universities in your pocket.",
+              style: TextStyle(fontSize: 22, color: Colors.blue),),
             SizedBox(height: 20),
             TextFormField(
-              decoration: textInputDecoration.copyWith(labelText: 'Search'),
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: 'Search Courses',
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue, width: 32.0),
+                    borderRadius: BorderRadius.circular(15.0)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 32.0),
+                    borderRadius: BorderRadius.circular(15.0)),
+              ),
             )
           ],
         ),
@@ -28,13 +39,13 @@ class WelcomePage extends StatelessWidget {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => Authenticate(showSignIn: true),
+                  builder: (context) => Authenticate(showSignIn: false),
                 )
               );
             },
             elevation: 10,
             color: Colors.blue,
-            child: Text('Log in'),
+            child: Text('Create your Account'),
           ),
         ),
         SizedBox(width: 5),
@@ -43,15 +54,15 @@ class WelcomePage extends StatelessWidget {
           child: RaisedButton(
             onPressed: () {
               Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Authenticate(showSignIn: false),
-                )
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Authenticate(showSignIn: true),
+                  )
               );
             },
             elevation: 10,
             color: Colors.white,
-            child: Text('Register'),
+            child: Text('Log in'),
           ),
         ),
       ],
